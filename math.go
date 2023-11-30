7ddf637b37c7fef5874f6a631b6a9170d755845d
Juan Farro <Juan.Farro@wtwco.com>
2023-11-30 00:26:41 -0700

    better version to read input file

--- math.go
+++ math.go
@@ -1,156 +1,132 @@
 package cp
 
 func ExecInt(operation string, list ...int) int {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
 }
 
 func ExecLong(operation string, list ...int64) int64 {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
 }
 
 func ExecDouble(operation string, list ...float64) float64 {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
 }
 
 func ExecIntList(operation string, list []int) int {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
 }
 
 func ExecLongList(operation string, list []int64) int64 {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
 }
 
 func ExecDoubleList(operation string, list []float64) float64 {
-	isMin := operation == "min"
-	isMax := operation == "max"
-	min := list[0]
-	max := list[0]
-	for _, item := range list {
-		if isMin {
+	if operation == "min" {
+		min := list[0]
+		for _, item := range list {
 			if item < min {
 				min = item
 			}
 		}
-		if isMax {
+		return min
+	}
+	if operation == "max" {
+		max := list[0]
+		for _, item := range list {
 			if item > max {
 				max = item
 			}
 		}
-	}
-	if isMin {
-		return min
-	}
-	if isMax {
 		return max
 	}
 	panic("operation not supported")
