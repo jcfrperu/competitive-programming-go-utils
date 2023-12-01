ba2fdd7ba16920420b8d19e57879eb4564484a65
Juan Farro <Juan.Farro@wtwco.com>
2023-12-01 00:43:05 -0700

    adding support for Advent of Code (out put writer is not needed)

--- run.go
+++ run.go
@@ -11,6 +11,16 @@ func RunHackerRank(funcToRun func([]string, *bufio.Writer)) {
 	run(funcToRun, os.Stdin, os.Getenv("OUTPUT_PATH")) // config for hackerrank.com
 }
 
+func RunAdventOfCodeWithString(funcToRun func([]string), stringInput string) {
+	adapterFunc := func(lines []string, _ *bufio.Writer) { funcToRun(lines) }
+	run(adapterFunc, strings.NewReader(stringInput), "")
+}
+
+func RunAdventOfCodeWithFile(funcToRun func([]string), filePath string) {
+	adapterFunc := func(lines []string, _ *bufio.Writer) { funcToRun(lines) }
+	run(adapterFunc, openFile(filePath), "")
+}
+
 func RunWithString(funcToRun func([]string, *bufio.Writer), stringInput string) {
 	run(funcToRun, strings.NewReader(stringInput), "")
 }
