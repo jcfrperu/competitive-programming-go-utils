b57748d58a5dc5464e3d6125479287e3a0381b36
Juan Farro <Juan.Farro@wtwco.com>
2024-12-04 19:10:08 -0700

    adding IndexAt(string, string, int) method

--- types.go
+++ types.go
@@ -1,9 +1,18 @@
 package cp
 
-type MNode[T any] struct {
+type Node[T any] struct {
 	Value T
 	Row   int
 	Col   int
 }
 
-type Matrix[T any] [][]MNode[T]
+type Matrix[T any] struct {
+	Matrix  [][]Node[T]
+	RowSize int
+	ColSize int
+}
+
+type DataMatrix[T any] struct {
+	Matrix           Matrix[T]
+	TransposedMatrix Matrix[T]
+}
