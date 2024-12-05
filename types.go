0c91958ede2bcd5900b7a35ec0aa17e7abe6b606
Juan Farro <Juan.Farro@wtwco.com>
2024-12-04 20:02:47 -0700

    adding support for Node and Matrix

--- types.go
+++ types.go
@@ -6,13 +6,62 @@ type Node[T any] struct {
 	Col   int
 }
 
-type Matrix[T any] struct {
-	Matrix  [][]Node[T]
-	RowSize int
-	ColSize int
+type Matrix[T any] [][]Node[T]
+
+func (m Matrix[T]) RowSize() int {
+	return len(m)
 }
 
-type DataMatrix[T any] struct {
-	Matrix           Matrix[T]
-	TransposedMatrix Matrix[T]
+func (m Matrix[T]) ColSize() int {
+	return len(m[0])
+}
+
+func (m Matrix[T]) MatrixSize() (int, int) {
+	return m.RowSize(), m.ColSize()
+}
+
+func (m Matrix[T]) IsValidRow(rowIndex int) bool {
+	return rowIndex >= 0 && rowIndex < m.RowSize()
+}
+
+func (m Matrix[T]) IsValidCol(colIndex int) bool {
+	return colIndex >= 0 && colIndex < m.ColSize()
+}
+
+func (m Matrix[T]) IsValidRowRange(startRowIndex int, endRowIndex int) bool {
+	return m.IsValidRow(startRowIndex) && m.IsValidRow(endRowIndex)
+}
+
+func (m Matrix[T]) IsValidColRange(startColIndex int, endColIndex int) bool {
+	return m.IsValidCol(startColIndex) && m.IsValidCol(endColIndex)
+}
+
+func (m Matrix[T]) RowAt(rowIndex int) []Node[T] {
+	return m[rowIndex]
+}
+
+func (m Matrix[T]) ColumnAt(colIndex int) []Node[T] {
+	rowsNumber := m.ColSize()
+	col := make([]Node[T], rowsNumber)
+	for i := range col {
+		col[i] = m[i][colIndex]
+	}
+	return col
+}
+
+func (m Matrix[T]) Transpose() Matrix[T] {
+	rowsNumber := m.ColSize()
+	colsNumber := m.RowSize()
+	t := make([][]Node[T], rowsNumber)
+	for i := range t {
+		t[i] = make([]Node[T], colsNumber)
+		for j := 0; j < colsNumber; j++ {
+			t[i][j] = Node[T]{
+				Value: m[j][i].Value,
+				Row:   i,
+				Col:   j,
+			}
+		}
+	}
+	return t
 }
